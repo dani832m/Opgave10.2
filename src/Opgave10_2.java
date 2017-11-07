426996592309bb67ffd7b728098326b4cbb52217
Daniel alias Comment God <31951102+dani832m@users.noreply.github.com>
2017-11-07 10:14:39 +0100

    Kommentarer tilrettet

--- src/Opgave10_2.java
+++ src/Opgave10_2.java
@@ -1,30 +1,35 @@
-// Denne klasse indkapsler vores primitive datatyper + opretter metoder og constructors
+//Denne klasse indkapsler vores primitive datatyper, opretter metoder og constructors
 
 public class Opgave10_2 {
 
+    //Deklarerer variabler
     private String name;
     private int age;
-    private double weight; // i kilogram!
-    private double height; // i meter!
+    private double weight; //Angives i kilogram!
+    private double height; //Angives i meter!
 
-    public Opgave10_2(String name, int age, double weight, double height) { // Constructor der opfylder kravene i opg. 10,2
+    public Opgave10_2(String name, int age, double weight, double height) { //Constructor der opfylder kravene i opg. 10.2
         this.name = name;
         this.age = age;
         this.weight = weight;
         this.height = height;
     }
 
+    //Anden constructor der tager tre parametre ind
     public Opgave10_2(String name, double weight, double height) {
         this(name, 20, weight, height);
     }
 
+    //Metode til at udregne BMI
     public double getBMI() {
         double bmi = weight / (height * height);
         return bmi;
     }
 
+    //Metode der fortæller, om du er undervægtig, normalvægtig eller overvægtig
     public String getStatus() {
-        double bmi = getBMI();
+        double bmi = getBMI(); //Initialiserer variablen "bmi" med den reelle BMI
+        //If-statement der inddeler BMI i kategorier
         if (bmi < 18.5)
             return "- Undervægtig";
         else if (bmi < 25)
@@ -35,6 +40,7 @@ public class Opgave10_2 {
             return "- Fed";
     }
 
+    //Metoder der returnerer henholdsvis String, int og doubles
     public String getName() {
         return name;
     }
